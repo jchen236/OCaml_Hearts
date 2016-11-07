4fd753b8a5f09395402df4bccbff1be27446f784
Albert Leung <Albert@dhcp-mcfad-1054.redrover.cornell.edu>
2016-11-06 21:24:02 -0500

    added card to game engine

--- game_engine.mli
+++ game_engine.mli
@@ -1,8 +1,12 @@
 (* The engine intializes the game and runs the game loop *)
 module type Engine = sig
+
   (* an up-to-date screenshot of the game at any point in time. keeps track of
    * information about the players, current hand(s), turns, and rounds  *)
   type state
+
+  (* integer representation of a card *)
+  type card
 
   (* intialize generates, shuffles, and distribute the cards accordingly
    * and returns a game state with attributes required to start the game *)
@@ -23,6 +27,10 @@ module type Engine = sig
   (* takes in current state and sum the score for cards played for losing player
    * as well as update the state variable *)
   val calculate_turn_score : state -> (player, int) -> state
+
+  (* turns the integer representation of the card to the matching card for
+   * the player *)
+  val int_to_card : int -> card
 
   (* returns the player with the lowest score after a game of hearts is
    * completed *)
