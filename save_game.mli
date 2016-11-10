1f5a7cf088526b93257e0b845a8f2f35557abd29
Albert Leung <Albert@dhcp-mcfad-4062.redrover.cornell.edu>
2016-11-09 22:47:37 -0500

    save_game methods using Yojson

---
+++ save_game.mli
@@ -0,0 +1,16 @@
+(* Contains methods to parse json files as well as write them
+ * for data saving. Utilizies the YoJson module. *)
+
+(* Takes in a json file and parses using the Yojson.Basic.json type. Only
+ * creates a json data type that we can utilize for further processing. *)
+val parse : string -> json
+
+(* Takes in a json data type and parses data appropriately. Will consist of a
+ * name, value pair. Used when trying to login into a username. Will return
+ * an error message if name is not found. *)
+val retrieve_stats : json -> (string * int)
+
+(* Takes a username, password, and a state to record the player's current
+ * stats for usage in the game. Produces a json type. If there is no json file
+ * this method will also handle the creation of such file. *)
+val save_user : string -> string -> state -> json
