9e199504843af855df67288a3500582ed018e35c
mlelien <emily.c.lien@gmail.com>
2016-11-24 02:31:48 -0500

    fixed syntax errors

--- Player.ml
+++ Player.ml
@@ -1,4 +1,4 @@
-module type Player struct =
+module Player : Player = struct
 
 	let get_legal_moves card_desired card_list =
 		if card_desired = None then get_hand
@@ -6,5 +6,5 @@ module type Player struct =
 								if card.suit = card_desired.suit then card::playable_list else playable_list) [] card_list
 
 	let play_card card hand =
-		List.fold_left (fun new_hand cur_card = if cur_card = card then new_hand else cur_card::new_hand) [] hand
+		List.fold_left (fun new_hand cur_card -> if cur_card = card then new_hand else cur_card::new_hand) [] hand
 end
